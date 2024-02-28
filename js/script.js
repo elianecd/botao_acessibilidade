bb6c75e4e677afb8ec1b33a86af9f0f80822b397
Eliane Chen Dahab <eliane.dahab@planejamento.rj.gov.br.local>
2024-02-28 12:44:46 -0300

    mais explicativo

--- js/script.js
+++ js/script.js
@@ -35,21 +35,24 @@ document.addEventListener('DOMContentLoaded', function () {
     // Function to toggle dark mode
     function toggleDarkMode() {
         const body = document.body; // Obtém a referência ao elemento <body> do documento
-        body.classList.toggle('dark_mode');  // Alterna a classe 'dark_mode' no elemento <body> puxando do css
-        const theme = body.classList.contains('dark_mode') ? 'dark' : 'light'; // Verifica se a classe 'dark_mode' está presente no elemento <body> e define o tema (theme) com base nisso
-        localStorage.setItem('theme', theme); // Salva a preferência do usuário para o tema no armazenamento local
-    
+        body.classList.toggle('dark_mode');  // Alterna a classe 'dark_mode' no elemento <body> puxando do css. classList: É uma propriedade do Elemento HTML, que fornece acesso a uma lista de classes CSS associadas ao elemento.
+        const theme = body.classList.contains('dark_mode') ? 'dark' : 'light'; // Verifica se a classe 'dark_mode' está presente no elemento <body> e define o tema (theme) com base nisso. Se body.classList.contains('dark_mode') for verdadeiro (true), o código retornará a string 'dark'.
+        localStorage.setItem('theme', theme); // Salva a preferência do usuário para o tema no armazenamento local. O primeiro argumento representa o nome do item a ser armazenado ou a chave usada para identificar o valor armazenado. O segundo argumeno eh o valor armazenado.
+        // O segundo argumento eh o valor da const theme definida na linha anterior.
+
         // Atualiza o texto do botão de acordo com o tema atual
         var darkModeBtn = document.querySelector('.darkModeBtn');
         darkModeBtn.textContent = body.classList.contains('dark_mode') ? 'Modo Claro' : 'Modo Escuro'; //textContent está manipulando o conteúdo textual do elemento que foi selecionado
     }
-    
+    // A constante theme definida dentro da função toggleDarkMode() é usada para calcular o valor atual do tema do site com base no estado atual do elemento body do documento e armazenar esse valor no local storage do navegador.
+
     // Event listeners for dark mode buttons
     document.querySelector('.darkModeBtn').addEventListener('click', toggleDarkMode);
 
     window.addEventListener('load', () => { // Quando a página está completamente carregada, este código é executado.
         const theme = localStorage.getItem('theme'); // Recupera a preferência do usuário para o tema do armazenamento local
-
+        // A constante theme definida aqui dentro é usada para recuperar a preferência do usuário para o tema do site do local storage do navegador quando a página é carregada. O valor recuperado é então usado para definir o estado inicial do site quando a página é carregada.
+        
         if (theme === 'dark') {
             document.body.classList.add('dark_mode'); // Se a preferência for 'dark', adiciona a classe 'dark_mode' ao elemento <body>
         }
